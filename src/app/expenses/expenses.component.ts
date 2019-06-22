bdba719e7b45a362532c73c62fb2ab2e5ffc994f
mczu <14128860+mczu@users.noreply.github.com>
2019-06-22 03:14:04 +0200

    validation start

--- src/app/expenses/expenses.component.ts
+++ src/app/expenses/expenses.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Expense } from '../expense';
 import { ExpenseService } from '../expense.service';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-expenses',
@@ -14,32 +14,48 @@ export class ExpensesComponent implements OnInit {
 
   expenses: Expense[];
 
-  // form
-  form = new FormGroup({
-    id: new FormControl(''),
-    shop: new FormControl(''),
-    price: new FormControl(''),
-    date: new FormControl(''),
-    time: new FormControl(''),
-    paymentMethod: new FormControl(''),
-    document: new FormControl('')
-  });
+  expenseForm: FormGroup;
 
-  constructor(private expenseService: ExpenseService) {
+
+
+  constructor(private expenseService: ExpenseService, private formBuilder: FormBuilder) {
+    this.expenseForm = this.createFormGroup();
+  }
+  createFormGroup() {
+    return this.formBuilder.group({
+    // form
+      id: ['', [Validators.required, Validators.pattern('[0-9]')]],
+      shop: '',
+      price: '',
+      date: '',
+      time: '',
+      paymentMethod: '',
+      document: ''
+    });
+  }
+
+  get id() { return this.expenseForm.get('id'); }
+
+  sendData() {
+    console.warn(this.expenseForm.value);
+    this.expenseService.addExpense(this.expenseForm.value as Expense)
+      .subscribe(expense => {
+        this.expenses.push(expense);
+      });
   }
 
   getExpenses(): void {
     this.expenseService.getExpenses()
       .subscribe(expenses => this.expenses = expenses);
   }
-  add(shop: string): void {
-    shop = shop.trim();
-    if (!shop) { return; }
-    this.expenseService.addExpense({ shop } as Expense)
-      .subscribe(expense => {
-        this.expenses.push(expense);
-      });
-  }
+  // add(shop: string): void {
+  //   shop = shop.trim();
+  //   if (!shop) { return; }
+  //   this.expenseService.addExpense({ shop } as Expense)
+  //     .subscribe(expense => {
+  //       this.expenses.push(expense);
+  //     });
+  // }
   delete(expense: Expense): void {
     this.expenses = this.expenses.filter(h => h !== expense);
     this.expenseService.deleteExpense(expense).subscribe();
@@ -49,8 +65,8 @@ export class ExpensesComponent implements OnInit {
     this.getExpenses();
   }
 
-  sendData() {
-    console.log(this.form.value);
-  }
+  // sendData() {
+  //   console.log(this.form.value);
+  // }
 
 }
